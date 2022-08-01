b4228d18d75a405a4945bc5100ec2c0a263869f3
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2022-08-01 07:22:42 +0000

    Support both model types in test_onnx.py

--- RWKV-v3/test_onnx.py
+++ RWKV-v3/test_onnx.py
@@ -21,47 +21,73 @@ def sample_logits(out, temperature=1.0, top_p=0.7):
 	return torch.multinomial(probs, num_samples=1)[0]
 
 
+def onnx_gpt_run(ctx):
+	for i in range(64):
+		tgt = len(ctx)
+		ttx = [] + ctx
+
+		while len(ttx) < 767:
+			ttx.append( 0 )
+
+		inputs = { "idx": [ttx] }
+
+		# [1][1][767][50277]
+		outputs = session.run(output_names=["x"], input_feed=inputs)
+		state = outputs[0][0][tgt - 1]
+
+		char = sample_logits(state)
+		char = char.item()
+
+		lprint( tokenizer.decode(char) )
+		ctx.append(char)
+
+def onnx_rnn_run(ctx):
+	xx_att = torch.zeros(12, 768).tolist()
+	aa_att = torch.zeros(12, 768).tolist()
+	bb_att = torch.zeros(12, 768).tolist()
+	xx_ffn = torch.zeros(12, 768).tolist()
+
+	for i in range(64):
+		tgt = len(ctx)
+		ttx = [] + ctx
+
+		# RNN takes the very last token
+		# Pad the input from the front
+		while len(ttx) < 768:
+			ttx.insert(0, 0)
+
+		inputs = { "idx": ttx, "xx_att": xx_att, "aa_att": aa_att, "bb_att": bb_att, "xx_ffn": xx_ffn }
+
+		outputs = session.run(output_names=["x", "xx_att_r", "aa_att_r", "bb_att_r", "xx_ffn_r"], input_feed=inputs)
+		state = outputs[0] # [50277]
+
+		# [12][768]
+		xx_att = outputs[1]
+		aa_att = outputs[2]
+		bb_att = outputs[3]
+		xx_ffn = outputs[4]
+
+		char = sample_logits(state)
+		char = char.item()
+
+		lprint( tokenizer.decode(char) )
+		ctx.append(char)
+
+
 tokenizer = PreTrainedTokenizerFast(tokenizer_file="20B_tokenizer.json")
 session = InferenceSession("rwkv.onnx")
 
 text = """\nIn a shocking finding,"""
-
 ctx = tokenizer.encode(text)
-print("Tokens in context:", len(ctx))
 
-lprint( tokenizer.decode(ctx) )
-
-xx_att = torch.zeros(12, 768).tolist()
-aa_att = torch.zeros(12, 768).tolist()
-bb_att = torch.zeros(12, 768).tolist()
-xx_ffn = torch.zeros(12, 768).tolist()
-
-for i in range(64):
-	tgt = len(ctx)
-	ttx = []
-
-	for id in ctx:
-		ttx.append(id)
-
-	# RNN takes the very last token
-	# Pad the input from the front
-	while len(ttx) < 768:
-		ttx.insert(0, 0)
-
-	inputs = { "idx": ttx, "xx_att": xx_att, "aa_att": aa_att, "bb_att": bb_att, "xx_ffn": xx_ffn }
-
-	outputs = session.run(output_names=["x", "xx_att_r", "aa_att_r", "bb_att_r", "xx_ffn_r"], input_feed=inputs)
-	state = outputs[0] # [50277]
-
-	# [12][768]
-	xx_att = outputs[1]
-	aa_att = outputs[2]
-	bb_att = outputs[3]
-	xx_ffn = outputs[4]
-
-	char = sample_logits(state)
-	char = char.item()
-
-	lprint( tokenizer.decode(char) )
-	ctx.append(char)
+if len( session.get_outputs() ) == 1:
+	print("This is an RWKV_GPT()-style model")
+	print("Tokens in context:", len(ctx))
+	lprint( tokenizer.decode(ctx) )
+	onnx_gpt_run(ctx)
+else:
+	print("This is an RWKV_RNN()-style model")
+	print("Tokens in context:", len(ctx))
+	lprint( tokenizer.decode(ctx) )
+	onnx_rnn_run(ctx)
 
